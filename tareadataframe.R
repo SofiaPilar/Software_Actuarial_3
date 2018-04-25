099405e01b735e60bb77d191475a87b36dd58e39
Sofia Dominguez Sandoval <35700787+SofiaPilar@users.noreply.github.com>
2018-04-24 23:32:34 -0500

    Delete tareadataframe.R

--- tareadataframe.R
+++
@@ -1,6 +0,0 @@
-
-Carrospili1<- data.frame(mtcars)
-Carrospili1
-
-dput(Carrospili1, file="~/GitHub/Software_Actuarial_3/carrospili1.R")
-carrospili2 <- dget(file="~/GitHub/Software_Actuarial_3/carrospili1.R")
